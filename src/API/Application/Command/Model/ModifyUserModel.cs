f58fd196b6cdda8c0e1ef3efb9c948765d464b5d
xtrystin <kamsil12>
2023-05-21 01:52:58 +0200

    Allowed to modify selected user's properties

--- src/API/Application/Command/Model/ModifyUserModel.cs
+++ src/API/Application/Command/Model/ModifyUserModel.cs
@@ -2,7 +2,7 @@
 
 public class ModifyUserModel
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Description { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Description { get; set; }
 }
